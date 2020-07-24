24dbc2c9099fab6ea7566d6222ca733cbe64e294
Conni Simmons <connisimmons@gmail.com>
2020-07-23 18:38:46 -0600

    quick update

--- api/catalog/catalog.model.js
+++ api/catalog/catalog.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const catalogSchema = new mongoose.Schema({
         title: String,
         description: String,
-        link: String,
+        link: Array,
         displayname: String,
         archived: Boolean
       },
